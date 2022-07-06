8cf7ce104a5505b9b4ab2b907323bda35cd20205
yifongau <yifongau@gmail.com>
2022-07-06 15:34:40 +0200

    Naamgeving

--- src/main/java/com/stater/alfabet_backend/domain/Lemma.java
+++ src/main/java/com/stater/alfabet_backend/domain/Lemma.java
@@ -17,6 +17,38 @@ public class Lemma {
 	@ManyToOne
 	Letter l;
 
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getBeschrijving() {
+		return beschrijving;
+	}
+
+	public void setBeschrijving(String beschrijving) {
+		this.beschrijving = beschrijving;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public Letter getL() {
+		return l;
+	}
+
+	public void setL(Letter l) {
+		this.l = l;
+	}
+
 	// Getters and setters
 	
 
